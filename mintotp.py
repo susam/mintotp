3b1019025d1bae5d13e0213a6c5b971a31e92c91
Susam Pal <susam@susam.in>
2019-08-13 07:34:02 +0530

    Name variables as per RFC 4226 and RFC 6238 terms

--- mintotp.py
+++ mintotp.py
@@ -7,23 +7,23 @@ import sys
 import time
 
 
-def hotp(secret, counter, digits=6, digest='sha1'):
-    padding = '=' * ((8 - len(secret)) % 8)
-    secret_bytes = base64.b32decode(secret.upper() + padding)
+def hotp(key, counter, digits=6, digest='sha1'):
+    padding = '=' * ((8 - len(key)) % 8)
+    key_bytes = base64.b32decode(key.upper() + padding)
     counter_bytes = struct.pack(">Q", counter)
-    mac = hmac.new(secret_bytes, counter_bytes, digest).digest()
+    mac = hmac.new(key_bytes, counter_bytes, digest).digest()
     offset = mac[-1] & 0x0f
-    truncated = struct.unpack('>L', mac[offset:offset+4])[0] & 0x7fffffff
-    return str(truncated)[-digits:].rjust(digits, '0')
+    binary = struct.unpack('>L', mac[offset:offset+4])[0] & 0x7fffffff
+    return str(binary)[-digits:].rjust(digits, '0')
 
 
-def totp(secret, interval=30):
-    return hotp(secret, int(time.time() / interval))
+def totp(key, time_step=30):
+    return hotp(key, int(time.time() / time_step))
 
 
 def main():
-    for secret in sys.stdin:
-        print(totp(secret.strip()))
+    for key in sys.stdin:
+        print(totp(key.strip()))
 
 
 if __name__ == '__main__':
